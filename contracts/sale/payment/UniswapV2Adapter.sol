243a435fe6e4c94cd2853596b0f429ab80d996d3
Jan-Paul Azucena <jp@fuelpowered.com>
2020-10-01 12:52:42 -0700

    [CHANGED] Code style of the function structures in UniswapV2Adapter.sol

--- contracts/sale/payment/UniswapV2Adapter.sol
+++ contracts/sale/payment/UniswapV2Adapter.sol
@@ -17,17 +17,30 @@ contract UniswapV2Adapter {
 
     IUniswapV2Router public uniswapV2Router;
 
-    constructor(IUniswapV2Router uniswapV2Router_) public {
+    constructor(
+        IUniswapV2Router uniswapV2Router_
+    ) public {
         _setUniswapV2Router(uniswapV2Router_);
     }
 
-    function _sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+    function _sortTokens(
+        address tokenA,
+        address tokenB
+    ) internal pure returns (
+        address token0,
+        address token1
+    ) {
         require(tokenA != tokenB, 'UniswapV2Adapter: IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'UniswapV2Adapter: ZERO_ADDRESS');
     }
 
-    function _pairFor(address tokenA, address tokenB) internal view returns (address pair) {
+    function _pairFor(
+        address tokenA,
+        address tokenB
+    ) internal view returns (
+        address pair
+    ) {
         (address token0, address token1) = _sortTokens(tokenA, tokenB);
         pair = address(uint256(keccak256(abi.encodePacked(
                 hex'ff',
@@ -37,13 +50,21 @@ contract UniswapV2Adapter {
             ))));
     }
 
-    function _getReserves(address tokenA, address tokenB) internal view returns (uint256 reserveA, uint256 reserveB) {
+    function _getReserves(
+        address tokenA,
+        address tokenB
+    ) internal view returns (
+        uint256 reserveA,
+        uint256 reserveB
+    ) {
         (address token0,) = _sortTokens(tokenA, tokenB);
         (uint256 reserve0, uint256 reserve1,) = IUniswapV2Pair(_pairFor(tokenA, tokenB)).getReserves();
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
-    function _setUniswapV2Router(IUniswapV2Router uniswapV2Router_) internal {
+    function _setUniswapV2Router(
+        IUniswapV2Router uniswapV2Router_
+    ) internal {
         uniswapV2Router = uniswapV2Router_;
         emit UniswapV2RouterSet(uniswapV2Router_);
     }
@@ -53,7 +74,9 @@ contract UniswapV2Adapter {
         address tokenB,
         uint256 amountInTokenA,
         address to
-    ) internal returns (uint256 amount) {
+    ) internal returns (
+        uint256 amount
+    ) {
         address[] memory path = new address[](2);
         path[0] = tokenA;
         path[1] = tokenB;
