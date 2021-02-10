9b8f874b26dc224c5434996541e84f6c737d5c10
Jan-Paul Azucena <jp@fuelpowered.com>
2021-02-10 11:02:28 -0800

    [REMOVED] Unnecessary openzeppelin and truffle configuration files

--- truffle-config.js
+++
@@ -1,6 +0,0 @@
-const config = require('@animoca/ethereum-contracts-core_library/truffle-config');
-
-// https://github.com/trufflesuite/truffle/issues/2688#issuecomment-736639231
-config.networks.ganache.disableConfirmationListener = true;
-
-module.exports = config;
